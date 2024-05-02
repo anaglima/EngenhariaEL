c8f8fa6b3a7de91e6ff3dba91c1d4b3f1b057525
anaglima <analima.dev@gmail.com>
2024-05-02 12:44:12 -0300

    primeiro commit

---
+++ backend/routes/user.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const connection = require("../connection");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+router.post("/signup", (req, res) => {
+  let user = req.body;
+  query = "select email,password,role,status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0) {
+        query =
+          "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false', 'user')";
+        connection.query(
+          query,
+          [user.name, user.contactNumber, user.email, user.password],
+          (err, results) => {
+            if (!err) {
+              return res
+                .status(200)
+                .json({ message: "Usuário registrado com sucesso!" });
+            } else {
+              return res.status(500).json(err);
+            }
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Este email já está cadastrado." });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  query = "select email,password,role,status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0 || results[0].password != user.password) {
+        return res
+          .status(401)
+          .json({ message: "Usuário ou senha incorretos." });
+      } else if (results[0].status === "false") {
+        return res
+          .status(401)
+          .json({ message: "Aguarde a aprovação do administrador!" });
+      } else if (results[0].password == user.password) {
+        const response = { email: results[0].email, role: results[0].role };
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+          expiresIn: "8h",
+        });
+        res.status(200).json({ token: accessToken });
+      } else {
+        return res.status(400).json({
+          message: "Algo deu errado. Por favor, tente novamente mais tarde.",
+        });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+router.post("/forgotPassword", (req, res) => {
+  const user = req.body;
+  query = "select email,password from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0) {
+        return res
+          .status(200)
+          .json({ message: "Senha enviada com sucesso para o seu e-mail!" });
+      } else {
+        var mailOptions = {
+          from: process.env.EMAIL,
+          to: results[0].email,
+          subject: "Password by Engenharia EL",
+          html:
+            "<p><b>Your Login details for Engenharia EL</b><br><b>Email:</b>" +
+            results[0].email +
+            "<br><b>Password: </b>" +
+            results[0].password +
+            '<br><a href="http://localhost:4200">Clique aqui para fazer o login</a></p>',
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+        return res
+          .status(200)
+          .json({ message: "Senha enviada com sucesso para o seu e-mail!" });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+module.exports = router;
---
+++ backend/table.sql
@@ -0,0 +1,16 @@
+create database engenhariael;
+
+use engenhariael;
+
+create table user(
+    id int primary key AUTO_INCREMENT,
+    name varchar(250),
+    contactNumber varchar(20),
+    email varchar(50),
+    password varchar(250),
+    status varchar(20),
+    role varchar(20),
+    UNIQUE (email)
+);
+
+insert into user(name,contactNumber,email,password,status,role) values('Admin', '16999999999', 'analima.dev@gmail.com', 'admin', 'true', 'admin');
