980dab4169c9fa0fbf3b762b1b7cd63d26fcec2d
anaglima <analima.dev@gmail.com>
2024-05-15 20:45:26 -0300

    up

--- backend/table.sql
+++ backend/table.sql
@@ -14,3 +14,32 @@ create table user(
 );
 
 insert into user(name,contactNumber,email,password,status,role) values('Admin', '16999999999', 'analima.dev@gmail.com', 'admin', 'true', 'admin');
+
+create table category(
+    id int not NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    primary key(id)
+);
+
+create table product(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    categoryId integer NOT NULL,
+    description varchar(255),
+    price integer,
+    status varchar(20),
+    primary key
+);
+
+create table bill(
+    id int NOT NULL AUTO_INCREMENT,
+    uuid varchar(200) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    contactNumber varchar(20) NOT NULL,
+    paymentMethod varchar(50) NOT NULL,
+    total int NOT NULL,
+    productDetails JSON DEFAULT NULL,
+    createdBy varchar(255) NOT NULL, 
+    primary key(id)
+)
