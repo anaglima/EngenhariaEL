94a208a77f687063748678bed61434373ea006c0
Stéfani Souza <souzastefani25@gmail.com>
2024-05-17 11:54:21 -0300

    agora vai

--- backend/connection.js
+++ backend/connection.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 var connection = mysql.createConnection({
-  port: 4200,
-  host: localhost,
-  user: root,
-  password: 120504,
-  database: engenhariael,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
